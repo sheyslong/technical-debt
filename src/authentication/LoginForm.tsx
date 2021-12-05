231f2b74a025a50b92311fd4bed842e2c3641777
Sheilla <sheilla.silva@ccc.ufcg.edu.br>
2021-12-05 08:17:23 -0300

    feat: save github user in sessionStorage

--- src/authentication/LoginForm.tsx
+++ src/authentication/LoginForm.tsx
@@ -1,17 +1,13 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { useForm } from 'react-hook-form'
-import { githubApi } from "../github/api/githubApi";
-import { useRepositoriesByUser } from "../github/hooks/useRepositoriesByUser";
-
 interface User {
     githubCredential: string
 }
 export const LoginForm:FC = () => {
     const { handleSubmit, register } = useForm<User>()
-    const [userRepositories, setUserRepositories] = useState<any>()
 
     const onSubmit = (userData: User) => {
-        //setUserRepositories(userRepositoriesDetails)        
+        window.sessionStorage.setItem('githubUser', userData.githubCredential)
     }
 
     return <form onSubmit={handleSubmit(onSubmit)}>
--- src/github/hooks/useRepositoriesByUser.ts
+++ src/github/hooks/useRepositoriesByUser.ts
@@ -2,7 +2,8 @@ import { useQuery } from "react-query"
 import { githubApi } from "../api/githubApi"
 
 export const useRepositoriesByUser = () => {
-    const userName = 'sheyslong'
+    const isWindowIsNotUndefined = typeof window !== 'undefined'
+    const userName = isWindowIsNotUndefined ? window.sessionStorage.getItem('githubUser') : ''
     const { data, isError, error, isLoading } = useQuery(
         ['repositories', userName], () => githubApi.getRepositoriesByUser(userName))
     console.log(data);
