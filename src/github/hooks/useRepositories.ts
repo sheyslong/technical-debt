8e4938cc75866deba657ba3ac3af624761b7aed2
Sheilla <sheilla.silva@ccc.ufcg.edu.br>
2021-12-07 09:41:24 -0300

    feat: return empty list when no github user

--- src/github/hooks/useRepositories.ts
+++ src/github/hooks/useRepositories.ts
@@ -3,9 +3,9 @@ import { githubApi } from "../api/githubApi"
 
 export const useRepositories = () => {
     const isWindowIsNotUndefined = typeof window !== 'undefined'
-    const userName = isWindowIsNotUndefined ? window.sessionStorage.getItem('githubUser') : ''
+    const userName = isWindowIsNotUndefined ? window.sessionStorage.getItem('githubUser') : undefined
     const { data, isError, error, isLoading } = useQuery(
         ['repositories', userName], () => githubApi.getRepositoriesByUser(userName))
     
-    return {repositories: data, isLoading, isError, error}
+    return {repositories: userName ? data : [], isLoading, isError, error}
 }
